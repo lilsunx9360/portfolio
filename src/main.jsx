3a611a01cd27eefbca5aa70b65ae6b664250fc88
lilsunx <your-email@example.com>
2025-05-21 11:00:53 +0530

    your message

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './Header.jsx'
-import Home from './home.jsx'
+import Home from './Homepage.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
